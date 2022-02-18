38f257dd4296faa94cb48c73442576527e8b3a72
harrishalisha <noranalisha@gmail.com>
2022-02-18 07:49:44 +0000

    React Router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+function Home() {
+    return (
+      <h1>HomePageComponent</h1>
+    )
+}
+ export default Home;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
+    Card, CardImg, CardImgOverlay,
     CardTitle
 } from 'reactstrap';
-import DishDetail from './DishdetailComponent ';
+//import DishDetail from './DishdetailComponent ';
 
 function RenderMenuItem(props) {
-    const { dish, onClick } = props;
+    const { dish } = props;
     return (
-        <Card key={dish.id}
-            onClick={() => onClick(dish.id)}>
+        <Card key={dish.id}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
@@ -23,7 +22,7 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         );
     });
